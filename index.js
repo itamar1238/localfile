51393423180ea38835e0bc7194220970f1076e45
AceTCC <tschapman@eths202.org>
2024-09-18 10:32:40 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Luh's Games</title>
+     <link rel="icon" href="select_mario.png" type="image/png">
+    <style>
+        body {
+            font-family: Impact, Impact;
+            text-align: center;
+            margin-top: 50px;
+        }
+        button {
+            font-size: 18px;
+            padding: 10px 20px;
+            margin: 10px;
+            cursor: pointer;
+            border: none;
+            border-radius: 5px;
+            background-color: #007BFF;
+            color: white;
+        }
+        button:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body> 
+<header>
+    <img src="select_mario.png" alt="Logo">
+    
+</header>
+
+    <h1>Luh's Games ;)</h1>
+    <h1>Luh's Games ;)</h1>
+    <h1>Luh's Games ;)</h1>
+    <button onclick="window.location.href='tapgame.html'">Tap Game</button>
+    <button onclick="window.location.href='SuperMario.html'">Super Mario Bros</button>
+    <button onclick="window.location.href='Eaglercraft.html'">Minecraft</button>
+    <button
+onclick="window.location.href='ss.html'">Super Searcher (site must be iframable)</button>
+     <button
+onclick="window.location.href='page5.html'">Go to Page 5</button>
+     <button
+onclick="window.location.href='page6.html'">Go to Page 6</button>
+     <button
+onclick="window.location.href='page8.html'">Go to Page 8</button>
+    <button
+onclick="window.location.href='page9.html'">Go to Page 9</button>
+
+    <button
+onclick="window.location.href='page10.html'">Go to Page 10</button>
+
+    <button
+onclick="window.location.href='page11.html'">Go to Page 11</button>
+
+    <button
+onclick="window.location.href='page12.html'">Go to Page 12</button>
+
+
+        
+    
+</body>
+</html>
